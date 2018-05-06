cd61744d1df406ad39bbaf13f822d8aa149c51cd
Magdalena Gresk <magdagresk@onet.pl>
2018-05-05 23:42:39 -0400

    Add formidable, modify js and html files

--- modules/handlers.js
+++ modules/handlers.js
@@ -2,7 +2,7 @@ var fs = require('fs')
 var formidable = require('formidable')
 
 exports.upload = function(request, response) {
-    console.log("Rozpoczynam obsługę żądania upload.")
+    console.log("Starting upload request.")
     var form = new formidable.IncomingForm()
     form.parse(request, function(error, fields, files) {
         fs.renameSync(files.upload.path, "test.png")
@@ -15,7 +15,7 @@ exports.upload = function(request, response) {
 
 
 exports.welcome = function(request, response) {
-    console.log("Rozpoczynam obsługę żądania welcome.")
+    console.log("Starting welcome request.")
     fs.readFile('templates/start.html', function(err, html) {
         response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"})
         response.write(html)
@@ -25,7 +25,7 @@ exports.welcome = function(request, response) {
 
 
 exports.error = function(request, response) {
-    console.log("Nie wiem co robić.")
+    console.log("I don't know what to do.")
     response.write("404 :(")
     response.end()
 }
--- modules/server.js
+++ modules/server.js
@@ -1,16 +1,16 @@
 var http = require('http')
 var colors = require('colors')
 
-var handlers = require('./handlers') // nasz moduł
+var handlers = require('./handlers') 
 
 function start() {
   function onRequest(request, response) {
-    console.log("Odebrano zapytanie.".green)
-    console.log("Zapytanie " + request.url + " odebrane.")
+    console.log("Request recived.".green)
+    console.log("Request " + request.url + " recived.")
 
     response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"})
 
-    switch (request.url) { // switch rozróżniający zapytania
+    switch (request.url) { 
         case '/':
         case '/start':
             handlers.welcome(request, response)
@@ -30,7 +30,7 @@ function start() {
 
   http.createServer(onRequest).listen(9000)
 
-  console.log("Uruchomiono serwer!".green)
+  console.log("Server started!".green)
 }
 
 exports.start = start
